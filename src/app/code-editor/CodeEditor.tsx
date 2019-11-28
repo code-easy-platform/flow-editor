6e1f6090e9d49d8cc8e612a0681fc3aa3712cbad
Lucas de Vargas Souza <lvargas@gvdasa.local>
2019-11-28 18:37:34 -0300

    Muitas melhorias no drag and drop. Funciona muito bem.

--- src/app/code-editor/CodeEditor.tsx
+++ src/app/code-editor/CodeEditor.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { useDrop, DropTargetMonitor, XYCoord } from 'react-dnd';
+import { useDrop, DropTargetMonitor } from 'react-dnd';
 
 import { ItemToDrag } from './components/item-drag/ItemDrag';
 import { ItemFluxo, ItemType } from './interfaces/ItemFluxo';
@@ -18,14 +18,13 @@ const itens: ItemFluxo[] = [
 ];
 
 const itensLogica: ItemFluxo[] = [
-    { id: 1, sucessorId: 2, nome: "Assign", top: 100, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, },
-    { id: 2, sucessorId: 3, nome: "Assign", top: 200, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, },
-    { id: 3, sucessorId: 4, nome: "Assign", top: 300, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, },
-    { id: 4, sucessorId: 5, nome: "Assign", top: 400, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, },
-    { id: 5, sucessorId: 6, nome: "Assign", top: 500, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, },
-    { id: 6, sucessorId: 7, nome: "Assign", top: 600, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, },
-    { id: 7, sucessorId: 8, nome: "Assign", top: 700, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, },
-    { id: 8, sucessorId: 0, nome: "Assign", top: 800, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, },
+    { id: 1, sucessorId: 2, nome: "START", top: 100, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, },
+    { id: 2, sucessorId: 3, nome: "IF", top: 200, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, },
+    { id: 3, sucessorId: 4, nome: "FOREACH", top: 300, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, },
+    { id: 4, sucessorId: 5, nome: "ACTION", top: 400, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, },
+    { id: 5, sucessorId: 6, nome: "SWICTH", top: 500, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, },
+    { id: 6, sucessorId: 7, nome: "ASSIGN", top: 600, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, },
+    { id: 7, sucessorId: 8, nome: "END", top: 700, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, },
 ];
 
 export const CodeEditor = (props: any) => {
@@ -36,33 +35,32 @@ export const CodeEditor = (props: any) => {
         svgHeight: flowItens.sort((a, b) => b.top - a.top)[0].top + 200,
         svgWidth: flowItens.sort((a, b) => b.left - a.left)[0].left + 200,
     });
-
     const [, drop] = useDrop({
         accept: [ItemType.ASSIGN],
         drop(item: any, monitor: DropTargetMonitor) {
+
             const target: any = svgRef.current;
             const targetSize: any = target.getBoundingClientRect();
             const draggedOffSet: any = monitor.getClientOffset();
-            const targetOffsetY: number = draggedOffSet.y + (targetSize.top - targetSize.top - targetSize.top);
+            const targetOffsetY: number = ((draggedOffSet.y) + (targetSize.top - targetSize.top - targetSize.top) - 25);
+            const targetOffsetX: number = ((draggedOffSet.x) + (targetSize.left - targetSize.left - targetSize.left) - 25);
 
-            const delta = monitor.getDifferenceFromInitialOffset() as XYCoord;
-            const left = Math.round((item.itemProps.left || 0) + delta.x);
-            const top = Math.round((item.itemProps.top || 0) + targetOffsetY - 40);
-
-            let _flowItens = flowItens;
-
-            _flowItens.push({
+            flowItens.push({
                 sucessorId: item.itemProps.sucessorId,
                 itemType: ItemType.ASSIGN,
                 nome: item.itemProps.nome,
                 id: Utils.getRandomId(10000, 100000000),
-                left: left,
+                left: targetOffsetX,
+                top: targetOffsetY,
                 height: 50,
                 width: 50,
-                top: top,
             });
 
-            setFlowItens(_flowItens);
+            setFlowItens(flowItens);
+            setSvgSize({
+                svgHeight: flowItens.sort((a, b) => b.top - a.top)[0].top + 200,
+                svgWidth: flowItens.sort((a, b) => b.left - a.left)[0].left + 200,
+            });
         },
     });
 
@@ -95,7 +93,7 @@ export const CodeEditor = (props: any) => {
     }
 
     return (
-        <div style={{ flex: 1, maxHeight: "100%", }}>
+        <div style={{ flex: 1, maxHeight: "100%" }}>
             <div className="mini-scroll-bar" style={{ padding: "10px", flexDirection: "column", overflow: "auto", alignItems: "center", width: 35, height: "100%", borderWidth: 0, borderRightWidth: 0.5, borderColor: "#949494bf", borderStyle: "solid" }}>
                 {itensLogica.map((item) => {
                     return <ItemToDrag
@@ -146,146 +144,3 @@ export const CodeEditor = (props: any) => {
         </div>
     );
 }
-
-
-
-
-
-
-
-    //#region Versão antiga
-
-/* const codeEditorContext = useContext(CodeEditorContext);
-const tabIndex: number = codeEditorContext.project.tabs.findIndex((tab: Tab) => { return tab.configs.isEditando === true ? tab : undefined });
-let listItens: Component[] = [];
-
-if (tabIndex >= 0) // Caso não exista uma tab selecionada.
-    listItens = codeEditorContext.project.tabs[tabIndex].itens;
-
-// Encontra o item que está sendo selecionado para ser editado!
-const itemEditando: Component = listItens.find((item: Component) => {
-    if (
-        (
-            item.configs.type === ComponentType.globalAction ||
-            item.configs.type === ComponentType.localAction
-        ) && item.configs.isEditando === true
-    ) return item; else return undefined;
-}) || EMPTY_COMPONENT;
-
-// Verifica se estou esditando alguma coisa.
-const isEditandoSomething = listItens.length > -1 && tabIndex >= 0;
-
-let fluxoList: Component[] = listItens.filter((item: Component) => { return item.configs.type === ComponentType.flowItem && item.paiId === itemEditando.id });
-
-
-
-const changeComponentState = (id: number, component: Component) => {
-    if (isEditandoSomething)
-        codeEditorContext.changeComponentState(id, component);
-}
-
-// Muda a posição do item de fluxo. Função passada por parâmetro para o itemDrag.
-const positionChange = (position: any) => { // Position recebe: { itemId, top, left }
-    const top = position.top;
-    const left = position.left;
-
-    let component: Component = fluxoList[fluxoList.findIndex((item: any) => { if (item.id === position.itemId) return item; return undefined; })];
-
-    component.top = top;
-    component.left = left;
-
-    changeComponentState(position.itemId, component);
-}
-
-// Permite ser possível soltar im item no fluxo.
-const [, drop] = useDrop({
-    accept: [
-        ComponentType.outputVariable,
-        ComponentType.inputVariable,
-        ComponentType.localVariable,
-        ComponentType.globalAction,
-        ComponentType.localAction,
-        ComponentType.flowItem,
-        ComponentType.rota,
-        FluxoComponentTypes.flowItem,
-    ],
-    drop(item: any, monitor: DropTargetMonitor) {
-        if (item.itemDetail.id) return;
-
-        const delta = monitor.getDifferenceFromInitialOffset() as XYCoord;
-        const left = Math.round((item.itemDetail.left || 0) + delta.x - 150);
-        const top = Math.round((item.itemDetail.top || 0) + delta.y - 40);
-
-        const newComponent: Component = codeEditorContext.addComponent(itemEditando.id, item.itemDetail.title, ComponentType.flowItem, 80, 80, top, left);
-
-        item.itemDetail.id = newComponent.id;
-        item.itemDetail.title = item.title + newComponent.id;
-    },
-});
-
-// Permite referenciar um item deste componente a partir de outro.
-const ref = useRef<HTMLInputElement>(null);
-
-// Agrupa as referencias do drop com as da ref.
-drop(ref);
-
-// Muda o sucessor do item que está sendo recebido por parâmetro.
-const onSucessorChange = (itemId: number, sucessorId: string) => {
-    let itemCurrent: Component = fluxoList[fluxoList.findIndex((item: Component) => { if (item.id === itemId) return item; else return undefined; })];
-    let itemSucessor: Component = fluxoList[fluxoList.findIndex((item: Component) => { if (item.id === Number(sucessorId)) return item; else return undefined; })];
-
-    // Propriedades do sucessor
-    itemCurrent.sucessorId = Number(sucessorId);
-    itemCurrent.isHaveSucessor = true;
-
-    // Propriedades do antecessor
-    itemSucessor.sucessorId = itemId;
-    itemSucessor.isHaveAntecessor = true;
-
-    // OBS: O update no fluxo é feito pela referencia entre variáveis js.
-
-    changeComponentState(itemCurrent.id, itemCurrent);
-    changeComponentState(itemSucessor.id, itemSucessor);
-}
-
-return (
-    isEditandoSomething
-        ? <div ref={ref} style={{ width: '100%' }}>
-            <svg style={{ width: '100%' }}>
-
-                {fluxoList.map((item: Component) => {
-                    const { id, title, left, top, width, height, sucessorId  } = item;
-
-                    let top2 = 0;
-                    let left2 = 0;
-                    let width2 = 0;
-                    try {
-                        let itemSucessor: Component = fluxoList[fluxoList.findIndex((item: Component) => { if (item.id === Number(sucessorId)) return item; else return undefined; })];
-
-                        top2 = itemSucessor.top;
-                        left2 = itemSucessor.left;
-                        width2 = itemSucessor.width;
-
-                    } catch (e) { }
-
-                    return <ItemToDrag
-                        id={id}
-                        top={top}
-                        left={left}
-                        title={title}
-                        width={width}
-                        height={height}
-                        refItemPai={ref}
-                        lineTargetLeft={left2 + (width2 / 2)}
-                        lineTargetTop={top2 - 5}
-                        onSucessorChange={onSucessorChange}
-                        outputPosition={positionChange}
-                    >{title}</ItemToDrag>;
-                })}
-
-            </svg>
-        </div>
-        : <div></div>
-) */
-    //#endregion
-
