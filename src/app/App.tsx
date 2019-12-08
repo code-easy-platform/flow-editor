0fddceb61ba86a2190050ab4bad9578a5c6091ef
Lucas Souza <lucas@magnastud.io>
2019-12-08 17:34:00 -0300

    Melhorias gerais no código!

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,12 +4,34 @@ import HTML5Backend from 'react-dnd-html5-backend';
 
 import { CodeEditor } from './code-editor/CodeEditor';
 import './App.scss';
+import { ItemType } from './code-editor/interfaces/ItemFluxo';
+
+
+const itens: any[] = [
+    { id: 1, sucessorId: 2, nome: "START", top: 100, left: 80, width: 50, height: 50, itemType: ItemType.START, isSelecionado: false },
+    { id: 2, sucessorId: 3, nome: "IF", top: 200, left: 80, width: 50, height: 50, itemType: ItemType.IF, isSelecionado: false },
+    { id: 3, sucessorId: 4, nome: "FOREACH", top: 300, left: 80, width: 50, height: 50, itemType: ItemType.ASSIGN, isSelecionado: false },
+    { id: 4, sucessorId: 5, nome: "ACTION", top: 400, left: 80, width: 50, height: 50, itemType: ItemType.ASSIGN, isSelecionado: false },
+    { id: 5, sucessorId: 6, nome: "SWICTH", top: 500, left: 80, width: 50, height: 50, itemType: ItemType.ASSIGN, isSelecionado: false },
+    { id: 6, sucessorId: 7, nome: "ASSIGN", top: 600, left: 80, width: 50, height: 50, itemType: ItemType.ASSIGN, isSelecionado: false },
+    { id: 7, sucessorId: 0, nome: "END", top: 800, left: 80, width: 50, height: 50, itemType: ItemType.END, isSelecionado: false },
+];
+const itensLogica: any[] = [
+    { id: 1, sucessorId: 2, top: 100, left: 80, width: 50, height: 50, nome: "START", itemType: ItemType.START, isSelecionado: false },
+    { id: 2, sucessorId: 3, top: 200, left: 80, width: 50, height: 50, nome: "IF", itemType: ItemType.IF, isSelecionado: false },
+    { id: 3, sucessorId: 4, top: 300, left: 80, width: 50, height: 50, nome: "FOREACH", itemType: ItemType.ASSIGN, isSelecionado: false },
+    { id: 4, sucessorId: 5, top: 400, left: 80, width: 50, height: 50, nome: "ACTION", itemType: ItemType.ASSIGN, isSelecionado: false },
+    { id: 5, sucessorId: 6, top: 500, left: 80, width: 50, height: 50, nome: "SWICTH", itemType: ItemType.ASSIGN, isSelecionado: false },
+    { id: 6, sucessorId: 7, top: 600, left: 80, width: 50, height: 50, nome: "ASSIGN", itemType: ItemType.ASSIGN, isSelecionado: false },
+    { id: 7, sucessorId: 8, top: 700, left: 80, width: 50, height: 50, nome: "END", itemType: ItemType.END, isSelecionado: false },
+];
+
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <DndProvider backend={HTML5Backend}>
-        <CodeEditor />
+        <CodeEditor itens={itens} toolItens={itensLogica} />
       </DndProvider>
     </div>
   );
--- src/app/code-editor/CodeEditor.tsx
+++ src/app/code-editor/CodeEditor.tsx
@@ -7,32 +7,27 @@ import { Line } from './components/lines/Line';
 import { Utils } from '../shared/Utils';
 import { Toolbar } from './components/tool-bar/ToolBar';
 
-const itens: ItemFluxo[] = [
-    { id: 1, sucessorId: 2, nome: "START", top: 100, left: 20, width: 50, height: 50, itemType: ItemType.START, isSelecionado: false },
-    { id: 2, sucessorId: 3, nome: "IF", top: 200, left: 20, width: 50, height: 50, itemType: ItemType.IF, isSelecionado: false },
-    { id: 3, sucessorId: 4, nome: "FOREACH", top: 300, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, isSelecionado: false },
-    { id: 4, sucessorId: 5, nome: "ACTION", top: 400, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, isSelecionado: false },
-    { id: 5, sucessorId: 6, nome: "SWICTH", top: 500, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, isSelecionado: false },
-    { id: 6, sucessorId: 7, nome: "ASSIGN", top: 600, left: 20, width: 50, height: 50, itemType: ItemType.ASSIGN, isSelecionado: false },
-    { id: 7, sucessorId: 0, nome: "END", top: 800, left: 20, width: 50, height: 50, itemType: ItemType.END, isSelecionado: false },
-];
+/**
+ * Propriedades aceitas pelo editor.
+ */
+export interface CodeEditorProps {
+    toolItens: ItemFluxo[],
+    itens: ItemFluxo[],
+}
 
-const itensLogica: ItemFluxo[] = [
-    { id: 1, sucessorId: 2, top: 100, left: 20, width: 50, height: 50, nome: "START", itemType: ItemType.START, isSelecionado: false },
-    { id: 2, sucessorId: 3, top: 200, left: 20, width: 50, height: 50, nome: "IF", itemType: ItemType.IF, isSelecionado: false },
-    { id: 3, sucessorId: 4, top: 300, left: 20, width: 50, height: 50, nome: "FOREACH", itemType: ItemType.ASSIGN, isSelecionado: false },
-    { id: 4, sucessorId: 5, top: 400, left: 20, width: 50, height: 50, nome: "ACTION", itemType: ItemType.ASSIGN, isSelecionado: false },
-    { id: 5, sucessorId: 6, top: 500, left: 20, width: 50, height: 50, nome: "SWICTH", itemType: ItemType.ASSIGN, isSelecionado: false },
-    { id: 6, sucessorId: 7, top: 600, left: 20, width: 50, height: 50, nome: "ASSIGN", itemType: ItemType.ASSIGN, isSelecionado: false },
-    { id: 7, sucessorId: 8, top: 700, left: 20, width: 50, height: 50, nome: "END", itemType: ItemType.END, isSelecionado: false },
-];
+/** Define quais itens são aceitos no drop do start. */
+const acceptedInDrop: ItemType[] = [ItemType.ASSIGN, ItemType.START, ItemType.END];
 
-// Define quais itens são aceitos no drop do start.
-const itensceitosNoDrop: ItemType[] = [ItemType.ASSIGN, ItemType.START, ItemType.END];
+/**
+ * Editor de fluxo.
+ * 
+ */
+export const CodeEditor: React.FC<CodeEditorProps> = ({ itens, toolItens }) => {
 
-export const CodeEditor = (props: any) => {
+    /** Referencia o svg onde está todos os itens de fluxo. */
+    const svgRef = useRef<any>(null);
 
-    const svgRef = useRef(null);
+    /** Controla o estado do editor inteiro. */
     const [state, setState] = useState({
         flowItens: itens,
         svgSize: { svgHeight: 0, svgWidth: 0 },
@@ -47,7 +42,7 @@ export const CodeEditor = (props: any) => {
 
     /** Usado para que seja possível o drop de itens no editor. */
     const [, dropRef] = useDrop({
-        accept: itensceitosNoDrop,
+        accept: acceptedInDrop,
         drop(item: any, monitor: DropTargetMonitor) {
 
             const target: any = svgRef.current;
@@ -158,6 +153,7 @@ export const CodeEditor = (props: any) => {
         setState({ ...state, flowItens: state.flowItens });
     }
 
+    /** Remove a selection da tela. */
     const removeSelection = () => {
         state.selectionProps = { isMouseDown: false, top:0, left: 0, finalTop:0, finalLeft: 0 };
         setState({ ...state, selectionProps: state.selectionProps });
@@ -167,6 +163,7 @@ export const CodeEditor = (props: any) => {
 
     /** Ativa a seleção na tela. */
     const exibiSelection = (event: any) => {
+        if (event.target.id !== svgRef.current.id) return;
 
         document.onmousemove = (event: any) => {
             if (state.selectionProps.isMouseDown) {
@@ -194,7 +191,6 @@ export const CodeEditor = (props: any) => {
             ...state,
             selectionProps: state.selectionProps
         });
-
     }
 
     /** Desabilita qualquer item que esteja selecionado. */
@@ -221,10 +217,10 @@ export const CodeEditor = (props: any) => {
 
     return (
         <div style={{ flex: 1, maxHeight: "100%" }}>
-            <Toolbar itensLogica={itensLogica} />
+            {(toolItens.length > 0) && <Toolbar itensLogica={toolItens} />}
 
-            <div key={"CodeEditor"} style={{ flex: 1, overflow: "auto", }}>
-                <svg tabIndex={0} ref={svgRef} onKeyDown={handleKeyPress} onMouseDown={onMouseDown} style={{
+            <div key={"CODE_EDITOR"} style={{ flex: 1, overflow: "auto", }}>
+                <svg tabIndex={0} id={"CODE_EDITOR_SVG"} ref={svgRef} onKeyDown={handleKeyPress} onMouseDown={onMouseDown} style={{
                     height: state.svgSize.svgHeight,
                     width: state.svgSize.svgWidth,
                     minHeight: "100%",
@@ -236,10 +232,10 @@ export const CodeEditor = (props: any) => {
                         stroke="#999fff"
                         strokeWidth={1}
                         onMouseUp={removeSelection}
-                        y={state.selectionProps.top}
+                        y={((state.selectionProps.finalTop - state.selectionProps.top) > 0) ? state.selectionProps.top : state.selectionProps.finalTop}
                         x={((state.selectionProps.finalLeft - state.selectionProps.left) > 0) ? state.selectionProps.left : state.selectionProps.finalLeft}
                         width={((state.selectionProps.finalLeft - state.selectionProps.left) > 0) ? (state.selectionProps.finalLeft - state.selectionProps.left) : (state.selectionProps.left - state.selectionProps.finalLeft)}
-                        height={state.selectionProps.finalTop - state.selectionProps.top}
+                        height={((state.selectionProps.finalTop - state.selectionProps.top) > 0) ? (state.selectionProps.finalTop - state.selectionProps.top) : (state.selectionProps.top - state.selectionProps.finalTop)}
                     />
 
                     {state.flowItens.map((item: ItemFluxo) => {
