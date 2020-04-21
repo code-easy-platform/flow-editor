96a2e06ac9653263b260422b91efe9c426a0834e
Lucas Souza <ls18017@gmail.com>
2020-04-21 10:56:32 -0300

    Correção do item de comentário; Adicionado a prop para desabilitar o selection; Prop no item de fluxo para exibição de erros; Ajustes gerais.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,18 +11,18 @@ const itens: FlowItem[] = [
   new FlowItem({ id: '3', sucessor: ['4'], top: 300, left: 80, width: 50, height: 50, name: "FOREACH", itemType: ItemType.FOREACH }),
   new FlowItem({ id: '4', sucessor: ['5'], top: 400, left: 80, width: 50, height: 50, name: "ACTION", itemType: ItemType.ACTION }),
   new FlowItem({ id: '5', sucessor: ['6'], top: 500, left: 80, width: 50, height: 50, name: "SWITCH", itemType: ItemType.SWITCH }),
-  new FlowItem({ id: '6', sucessor: ['7'], top: 600, left: 80, width: 50, height: 50, name: "ASSIGN", itemType: ItemType.ASSIGN }),
+  new FlowItem({ hasError: true, id: '6', sucessor: ['7'], top: 600, left: 80, width: 50, height: 50, name: "ASSIGN", itemType: ItemType.ASSIGN }),
   new FlowItem({ id: '7', sucessor: ['0'], top: 700, left: 80, width: 50, height: 50, name: "END", itemType: ItemType.END }),
   new FlowItem({ id: '8', sucessor: ['0'], top: 100, left: 200, width: 200, height: 100, name: "COMMENT", itemType: ItemType.COMMENT }),
 ];
 const itensLogica: FlowItem[] = [
-  new FlowItem({ id: '1', name: "START",   itemType: ItemType.START   }),
-  new FlowItem({ id: '2', name: "ACTION",  itemType: ItemType.ACTION  }),
-  new FlowItem({ id: '3', name: "IF",      itemType: ItemType.IF      }),
+  new FlowItem({ id: '1', name: "START", itemType: ItemType.START }),
+  new FlowItem({ id: '2', name: "ACTION", itemType: ItemType.ACTION }),
+  new FlowItem({ id: '3', name: "IF", itemType: ItemType.IF }),
   new FlowItem({ id: '4', name: "FOREACH", itemType: ItemType.FOREACH }),
-  new FlowItem({ id: '6', name: "SWITCH",  itemType: ItemType.SWITCH  }),
-  new FlowItem({ id: '7', name: "ASSIGN",  itemType: ItemType.ASSIGN  }),
-  new FlowItem({ id: '8', name: "END",     itemType: ItemType.END     }),
+  new FlowItem({ id: '6', name: "SWITCH", itemType: ItemType.SWITCH }),
+  new FlowItem({ id: '7', name: "ASSIGN", itemType: ItemType.ASSIGN }),
+  new FlowItem({ id: '8', name: "END", itemType: ItemType.END }),
   new FlowItem({ id: '9', name: "COMMENT", itemType: ItemType.COMMENT }),
 ];
 
@@ -34,10 +34,11 @@ const App: React.FC = () => {
       <FlowEditor
         itens={itens}
         allowDropTo={[]}
-        breadcrumbsPath="Routers/authenticate"
         isShowToolbar={true}
         toolItens={itensLogica}
         onChangeItens={outItens}
+        isDisabledSelection={false}
+        breadcrumbsPath="Routers/authenticate"
         onContextMenu={(data, e) => {
           console.log(data);
           console.log(e);
