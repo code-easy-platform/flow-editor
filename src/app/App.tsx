03356f9bc07e7a3514af8c66eab8e9415e70d9ab
lucassouza <ls18017@gmail.com>
2020-05-15 23:57:25 -0300

    fix(item drag): added the stopPropagation in context menu execution

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,13 +6,13 @@ import './App.css';
 
 
 const itens: FlowItem[] = [
-    new FlowItem({ id: '1', sucessor: ['2'], top: 100, left: 80, width: 50, height: 50, name: "START", itemType: ItemType.START }),
-    new FlowItem({ id: '2', sucessor: ['3', '0'], top: 200, left: 80, width: 50, height: 50, name: "IF", itemType: ItemType.IF }),
-    new FlowItem({ id: '3', sucessor: ['4'], top: 300, left: 80, width: 50, height: 50, name: "FOREACH", itemType: ItemType.FOREACH }),
-    new FlowItem({ id: '4', sucessor: ['5'], top: 400, left: 80, width: 50, height: 50, name: "ACTION", itemType: ItemType.ACTION }),
-    new FlowItem({ id: '5', sucessor: ['6'], top: 500, left: 80, width: 50, height: 50, name: "SWITCH", itemType: ItemType.SWITCH }),
-    new FlowItem({ id: '6', sucessor: ['7'], top: 600, left: 80, width: 50, height: 50, name: "ASSIGN", hasError: true, itemType: ItemType.ASSIGN }),
-    new FlowItem({ id: '7', sucessor: ['0'], top: 700, left: 80, width: 50, height: 50, name: "END", itemType: ItemType.END }),
+    new FlowItem({ id: '1', sucessor: ['2'], top: 100, left: 80, name: "START", itemType: ItemType.START }),
+    new FlowItem({ id: '2', sucessor: ['3', '0'], top: 200, left: 80, name: "IF", itemType: ItemType.IF }),
+    new FlowItem({ id: '3', sucessor: ['4'], top: 300, left: 80, name: "FOREACH", itemType: ItemType.FOREACH }),
+    new FlowItem({ id: '4', sucessor: ['5'], top: 400, left: 80, name: "ACTION", itemType: ItemType.ACTION }),
+    new FlowItem({ id: '5', sucessor: ['6'], top: 500, left: 80, name: "SWITCH", itemType: ItemType.SWITCH }),
+    new FlowItem({ id: '6', sucessor: ['7'], top: 600, left: 80, name: "ASSIGN", hasError: true, itemType: ItemType.ASSIGN }),
+    new FlowItem({ id: '7', sucessor: ['0'], top: 700, left: 80, name: "END", itemType: ItemType.END }),
     new FlowItem({ id: '8', sucessor: ['0'], top: 100, left: 200, width: 200, height: 100, name: "COMMENT", itemType: ItemType.COMMENT }),
 ];
 const itensLogica: FlowItem[] = [
@@ -32,11 +32,10 @@ const App: React.FC = () => {
             <FlowEditor
                 itens={itens}
                 id={"FLOW_EDITOR"}
+                showToolbar={true}
                 allowedsInDrop={[]}
                 key={"FLOW_EDITOR"}
-                showToolbar={true}
                 toolItens={itensLogica}
-                enabledSelection={true}
                 onChangeItens={console.log}
                 // onMouseOver={console.log}
                 breadcrumbs={[
