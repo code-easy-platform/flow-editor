bc359ae529a8d6a4ff072faba48e4265b46812df
lvargas <lvargas@gvdasa.com.br>
2021-06-14 13:34:32 -0300

    feat: added flow icons

---
+++ src/app/flow-editor/shared/assets/index.ts
@@ -0,0 +1,19 @@
+import IconIf from './flow-icons/if.svg';
+import IconEnd from './flow-icons/end.svg';
+import IconStart from './flow-icons/start.svg';
+import IconAction from './flow-icons/action.svg';
+import IconAssign from './flow-icons/assign.svg';
+import IconSwitch from './flow-icons/switch.svg';
+import IconForEach from './flow-icons/foreach.svg';
+import IconComment from './flow-icons/comment.png';
+
+export const DefaultFlowIcons = {
+    IconIf,
+    IconEnd,
+    IconStart,
+    IconAction,
+    IconAssign,
+    IconSwitch,
+    IconForEach,
+    IconComment,
+};
