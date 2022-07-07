13230f8a7f111d85d31ac2f0c5200a8cef7c47c9
Lucas Souza <lvargas@gvdasa.com.br>
2022-07-07 14:15:26 -0300

    refactor: improved flow lines

--- src/app/flow/shared/components/Line.tsx
+++ src/app/flow/shared/components/Line.tsx
@@ -30,49 +30,23 @@ export const Line: React.FC<IDraggableContainerProps> = ({ left1Observable, top1
   const inputSlot = useObserverValue(inputSlotObservable);
   const outputSlot = useObserverValue(outputSlotObservable);
 
+
   const resolvedLeft2 = useMemo(() => gridSnap(left2, snapGrid) - 10, [left2, snapGrid]);
   const resolvedLeft1 = useMemo(() => gridSnap(left1, snapGrid) + width1 + 10, [left1, width1, snapGrid]);
 
   const resolvedTop1 = useMemo(() => gridSnap(top1, snapGrid) + height1 - 7 - (outputSlot * 16), [top1, height1, outputSlot, snapGrid]);
   const resolvedTop2 = useMemo(() => gridSnap(top2, snapGrid) + 7 + (inputSlot * 16), [top2, inputSlot, snapGrid]);
 
-
-  const resolvedLeftMiddle = useMemo(() => (resolvedLeft2 - resolvedLeft1) * 0.5, [resolvedLeft1, resolvedLeft2]);
-  const resolvedTopMiddle = useMemo(() => (resolvedTop2 - resolvedTop1) * 0.5, [resolvedTop1, resolvedTop2]);
-
-  const diferenceLeft1Left2 = useMemo(() => (resolvedLeft2 - resolvedLeft1) * -1, [resolvedLeft1, resolvedLeft2]);
-
-  const resolvedQuadraticY = useMemo(() => {
-    if (diferenceLeft1Left2 <= 0) return 0;
-
-    if (diferenceLeft1Left2 > 25) return 25;
-
-    return diferenceLeft1Left2;
-  }, [diferenceLeft1Left2]);
-
-  const resolvedQuadraticX = useMemo(() => {
-    if (diferenceLeft1Left2 > 0 && diferenceLeft1Left2 < 200) return 100 + diferenceLeft1Left2;
-
-    if (diferenceLeft1Left2 > 200) return 100 + 200;
-
-    return 100;
-  }, [diferenceLeft1Left2]);
-
-
-
   const pathD = useMemo(() => {
-    //return `M${resolvedLeft1},${resolvedTop1} q${resolvedQuadraticX},${resolvedQuadraticY} ${resolvedLeftMiddle},${resolvedTopMiddle} T${resolvedLeft2},${resolvedTop2}`;
-    const start = `M${resolvedLeft1},${resolvedTop1} ${resolvedLeft1 + 15},${resolvedTop1}`;
+    const start = `M ${resolvedLeft1},${resolvedTop1} ${resolvedLeft1 + 5},${resolvedTop1}`;
+    const middle1 = `c 0,0 -10,0 10,0`;
+    const middle2 = `S ${resolvedLeft2 - 30},${resolvedTop2} ${resolvedLeft2 - 15},${resolvedTop2}`;
     const end = `M${resolvedLeft2 - 15},${resolvedTop2} ${resolvedLeft2},${resolvedTop2}`;
 
-    const middle1 = ``;
-    const middle2 = ``;
-    const middle3 = ``;
-    //const middle = `q${resolvedQuadraticX},${resolvedQuadraticY} ${resolvedLeftMiddle},${resolvedTopMiddle} T${resolvedLeft2 - 15},${resolvedTop2}`;
-    const middle = `q${0},${15} ${0},${-15}`;
 
-    return `${start} ${middle} ${end}`;
-  }, [resolvedLeft1, resolvedTop1, resolvedQuadraticX, resolvedQuadraticY, resolvedLeftMiddle, resolvedTopMiddle, resolvedLeft2, resolvedTop2]);
+    return `${start} ${middle1} ${middle2} ${end}`;
+  }, [resolvedLeft1, resolvedTop1, resolvedLeft2, resolvedTop2]);
+
 
   return (
     <path
