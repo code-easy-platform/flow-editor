47b1a83d761bf372ff97e942f8174af997ece648
lvsouza <ls18017@gmail.com>
2024-10-08 12:46:31 -0300

    refactor: improve example

--- example/src/Mock.tsx
+++ example/src/Mock.tsx
@@ -28,27 +28,34 @@ export const FLOW: INode[] = [
       {
         relatedId: forConnectionNode,
         id: observe(crypto.randomUUID()),
-        endHandleId: observe(undefined),
-        startHandleId: observe(undefined),
+        endHandleId: observe('true'),
+        startHandleId: observe('output'),
+      }
+    ]),
+  },
+  {
+    id: observe(crypto.randomUUID()),
+    height: observe(32),
+    width: observe(100),
+    left: observe(50),
+    top: observe(300),
+    render: props => <CustomNode {...props} type="text" />,
+    connections: observe<INodeConnection[]>([
+      {
+        relatedId: forConnectionNode,
+        id: observe(crypto.randomUUID()),
+        endHandleId: observe('false'),
+        startHandleId: observe('output'),
       }
     ]),
   },
   {
     id: forConnectionNode,
     connections: observe([]),
-    height: observe(32),
-    width: observe(100),
-    left: observe(50),
+    height: observe(70),
+    width: observe(70),
+    left: observe(300),
     top: observe(300),
     render: props => <CustomNode {...props} type="if" />,
-  },
-  {
-    id: observe(crypto.randomUUID()),
-    connections: observe([]),
-    height: observe(100),
-    width: observe(100),
-    left: observe(250),
-    top: observe(100),
-    render: props => <CustomNode {...props} type="text" />,
   },
 ];
--- example/src/app/components/CustomNode.tsx
+++ example/src/app/components/CustomNode.tsx
@@ -17,13 +17,16 @@ export const CustomNode = (props: INodeRenderProps & { type: 'text' | 'if' }) =>
         border: isSelected ? 'thin solid blue' : 'thin solid transparent',
       }}
     >
-      CustomNode
+      {props.type}
 
-      <div style={{ display: 'flex', justifyContent: 'end' }}>
-        <Handle
-          position={props.type === 'if' ? 'left' : 'right'}
-        />
-      </div>
+      <Handle id='output' position='right' />
+
+      {props.type === 'if' && (
+        <>
+          <Handle id='true' position='left' top={-10} />
+          <Handle id='false' position='left' top={10} />
+        </>
+      )}
     </div>
   );
 };
